42bcf0ee6dd136c8cb2191f59fe7abfec20bbc88
taruneluri <taruneluri2106@gmail.com>
2022-07-06 10:10:51 +0530

    first

---
+++ src/components/greet.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// function greet(){
+//     return <h2>Hello Tarun</h2>
+// }
+const greet=()=><h1>Hello Tarun</h1>;
+export default greet;
---
+++ src/components/welcome.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+class Welcome extends Component{
+    render(){
+        return <h2>Papaya component</h2>
+    }
+}
+export default Welcome;
