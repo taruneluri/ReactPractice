1fbdf757de1430d1f07dbf90a4387f5bb2799f42
taruneluri <taruneluri2106@gmail.com>
2022-07-06 10:32:41 +0530

    props basics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React from 'react';
 import Greet from './components/greet'
-import Welcome from './components/welcome';
-import Hello from './components/hello';
+//import Welcome from './components/welcome';
+//import Hello from './components/hello';
 function App() {
   return (
     <div className="App">
-      <Greet/><br/>
-      <Welcome/>
-      <Hello/>
+      <Greet name="Sasi"/><br/>
+      <Greet name="Srinija"/><br/>
+      <Greet name="Sindhu"/><br/>
+      
     </div>
   );
 }
--- src/components/greet.js
+++ src/components/greet.js
@@ -2,5 +2,8 @@ import React from 'react';
 // function greet(){
 //     return <h2>Hello Tarun</h2>
 // }
-const greet=()=><h1>Hello Tarun</h1>;
+const greet=(props)=>{
+    console.log(props)
+    return <h2>Hello {props.name}</h2>
+}
 export default greet;
